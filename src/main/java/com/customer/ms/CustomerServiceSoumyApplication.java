1444acd8a274493662946a436dc83a52b54909bc
k.b.soumya <k.b.soumya@CPX-1FZGGE32EVK.dir.svc.accenture.com>
2020-09-29 08:36:06 -0500

    Service Code

---
+++ src/main/java/com/customer/ms/CustomerServiceSoumyApplication.java
@@ -0,0 +1,13 @@
+package com.customer.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceSoumyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceSoumyApplication.class, args);
+	}
+
+}
